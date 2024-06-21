59b08f2aa07b4b192d5d36e5be07f79e9988baf2
Valery Rabchenyuk <valery0623@gmail.com>
2024-06-21 15:55:50 +0300

    major changes

--- the_snake.py
+++ the_snake.py
@@ -1,4 +1,4 @@
-from random import choice, randint
+from random import randint
 
 import pygame
 
@@ -27,7 +27,7 @@ APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
 # Скорость движения змейки:
-SPEED = 20
+SPEED = 10
 
 # Настройка игрового окна:
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -41,41 +41,46 @@ clock = pygame.time.Clock()
 
 # Тут опишите все классы игры.
 class GameObject:
-    def __init__(self):                            
+    """ Родительский класс """
+    def __init__(self):
+        """ 1 метод — инициализация """
         self.position = ((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
         self.body_color = None
 
     def draw(self):
+        """ Абстрактный метод для переопределения  """
         pass
 
 
 class Apple(GameObject):
-
+    """ Дочерний класс """
     def __init__(self, body_color=APPLE_COLOR):
-        """ 1 метод — инициализация """                        #проверить body_color=APPLE_COLOR  MOZNO UBRAT'
-        super().__init__()               #super.__init__(body_color=APPLE_COLOR)
-        self.body_color = body_color                      #=APPLE_COLOR    BILO bez position arg 
-        self.randomize_position()       
+        """ 1 метод — инициализация """
+        super().__init__()
+        self.body_color = body_color
+        self.randomize_position()
 
     def randomize_position(self):
+        """ 2 метод — случайное положение яблока """
         self.position = (
             randint(0, GRID_WIDTH) * GRID_SIZE,
             randint(0, GRID_HEIGHT) * GRID_SIZE,
         )
 
     def draw(self):
+        """ 3 метод — отрисовка яблока (прекод) """
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
 
 class Snake(GameObject):
-
+    """ Дочерний класс """
     def __init__(self, body_color=SNAKE_COLOR):
         """ 1 метод — инициализация """
         super().__init__()
         self.length = 1  # 1 атрибут — длинна
-        self.positions = [self.position]  # 2 атрибут — начало — центр экрана.
+        self.positions = [self.position]  # 2 атрибут — начало — центр экрана
         self.direction = RIGHT  # 3 атрибут — направление движения
         self.next_direction = None  # 4 атрибут — след. направление
         self.body_color = body_color  # 5 атрибут — цвет
@@ -89,7 +94,7 @@ class Snake(GameObject):
 
     def get_head_position(self):
         """ 3 метод — возвращает позицию головы """
-        return self.position[0]
+        return self.positions[0]
 
     def move(self):
         """ 4 метод — обновление позиции """
@@ -97,13 +102,14 @@ class Snake(GameObject):
         head_position = self.get_head_position()
         x, y = self.direction
         new_head = (
-            (head_position[0] + x * GRID_SIZE) % SCREEN_WIDTH
+            (head_position[0] + x * GRID_SIZE) % SCREEN_WIDTH,
             (head_position[1] + y * GRID_SIZE) % SCREEN_HEIGHT
         )
+
         if new_head in self.positions[2:]:
             self.reset()
         else:
-            self.positions.insert(0, new_head)
+            self.positions = [new_head] + self.positions
             if len(self.positions) > self.length:
                 self.last = self.positions.pop()
 
@@ -125,16 +131,17 @@ class Snake(GameObject):
             pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
 
     def reset(self):
-        """ 6 метод — сброс змеи """ 
+        """ 6 метод — сброс змеи """
         self.leght = 1
         self.positions = [self.position]
-        self.direction = RIGHT    #направление устанавливается случайным образом
+        self.direction = RIGHT
         self.next_direction = None
-        self.last = None 
+        self.last = None
         screen.fill(BOARD_BACKGROUND_COLOR)
 
+
 def handle_keys(game_object):
-    """ Обработка нажатия клавиш (прекод) """                 
+    """ Обработка нажатия клавиш (прекод) """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -149,56 +156,37 @@ def handle_keys(game_object):
             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
                 game_object.next_direction = RIGHT
 
+
 def main():
-    """Главный цикл игры"""
+    """ Главный цикл игры """
     # Инициализация PyGame:
     pygame.init()
     # Экземпляры классов
     snake = Snake()
     apple = Apple()
-
+  
     while True:
         clock.tick(SPEED)
-
         handle_keys(snake)
+
+        apple.draw()
+        snake.draw()
         snake.move()
 
-        snake.draw()
-        apple.draw()    
+        # Поедание яблока
         if snake.get_head_position() == apple.position:
             snake.length += 1
-        
-        pygame.display.updates()
+            apple.randomize_position()
 
+        # Обновление экрана
+        pygame.display.update()
+
+        # Завершение игры
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 break
 
-        #pygame.display.updates()   To li zdec' To li vishe
-        
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-# Функция обработки действий пользователя
-# def handle_keys(game_object):
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             raise SystemExit
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_UP and game_object.direction != DOWN:
-#                 game_object.next_direction = UP
-#             elif event.key == pygame.K_DOWN and game_object.direction != UP:
-#                 game_object.next_direction = DOWN
-#             elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
-#                 game_object.next_direction = LEFT
-#             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
-#                 game_object.next_direction = RIGHT
-
-
